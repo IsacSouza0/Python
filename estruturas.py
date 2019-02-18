0a7f3cf1080347a48d0c8fd86b9f1e13f6a8b08c
Beatriz <biiah_beatriz@hotmail.com>
2019-02-18 12:07:38 -0300

    Exercicios

---
+++ estruturas.py
@@ -0,0 +1,88 @@
+# if 2 is 3:
+#     print('dois')
+#     else:
+#         print('dois é igual a dois')
+
+# if 2 is 3:
+#      print('dois')
+# else:
+#      print('dois é igual a dois')
+
+
+# lista= ['a','b','c',1, 2, 3]
+# for item in lista:
+#     print(item)
+
+
+# string= 'Beatriz'
+# for letra in string:
+#     print(letra)
+
+
+
+# n1= int(input('digite o número 1:'))
+# n2= int(input('digite o número 2:'))
+
+# print(n1)
+# print(n2)
+
+# soma= n1 + n2
+# print(soma)
+
+
+# n1=int(input('digite o número 1:'))
+# n2=int(input('digite o número 2:'))
+
+# print(n1)
+# print(n2)
+
+# subtração= n1 - n2 
+# print(subtração) 
+
+# n1=int(input('digite o número 1:'))
+# n2=int(input('digite o número 2:'))
+
+# print(n1)
+# print(n2)
+
+# multiplicação= n1 * n2
+# print(multiplicação)
+
+
+# n1=int(input('digite o número 1:'))
+# n2=int(input('digite o número 2:'))
+
+# print(n1)
+# print(n2)
+
+# divisão= n1 / n2
+# print(divisão)
+
+# n1=float(input('digite o número 1:'))
+# n2=float(input('digite o número 2:'))
+# n3=float(input('digite o número 3:'))
+# n4=float(input('digite o número 4:'))
+
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+
+# soma= (n1 + n2 + n3 + n4)
+# print (soma)
+# print (soma/4)
+
+# n1=int(input('digite o número 1:'))
+# multiplicar=(n1 * 9/5) 
+
+# print(multiplicar)
+# print(multiplicar+32)
+
+
+# palavra=input('digite uma palavra:' )
+# letra=input('digite uma letra:')
+
+# contagem= palavra.count(letra)
+
+# print(f'A letra {letra} aparece {contagem} vezes na palavra {palavra}')
+
